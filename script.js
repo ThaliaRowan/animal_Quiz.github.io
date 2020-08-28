bd2b3d76f5284a0cb7c9bdc69e4b3183e041ea54
Thalia <thalia.rowan@gmail.com>
2020-08-27 22:12:42 -0300

    Added time and  q and A functions

---
+++ script.js
@@ -0,0 +1,87 @@
+var cardTitle = document.querySelector("#cardTitle");
+var question = document.querySelector("#question");
+var btn = document.querySelector("#btn");
+var opt = document.querySelector("#options");
+var button = document.createElement("button");
+var time = document.querySelector('#time');
+var idx;
+var opIdx;
+var counter = 0;
+var objArr = [];
+var ansArr = [];
+var secondsLeft = 90; 
+
+function myObj() {
+  for (let i = 0; i < options.length; i++) {
+    objArr.push(options[i]);
+  }
+  return objArr;
+}
+
+function myQuestion() {
+  idx = 0;
+  var que = objArr[idx];
+  question.innerHTML = que.question;
+
+  var cQuest = objArr.indexOf(que);
+  objArr.splice(cQuest, 1);
+
+  var answers = que.answer.length;
+
+  for (let i = 0; i < answers; i++) {
+    ansArr.push(i);
+  }
+   opt.innerHTML = '';
+  for (let i = 0; i < answers; i++) {
+    //opIdx = 0;
+    var optIdx = ansArr[idx];
+
+    var cAns = ansArr.indexOf(optIdx);
+    ansArr.splice(cAns, 1);
+    console.log(ansArr);
+
+    var sect = document.createElement("div");
+    sect.innerHTML = que.answer[i];
+    opt.appendChild(sect);
+  }
+
+  counter++;
+}
+
+
+function myTimer(){
+    var theInterval = setInterval(() => {
+        secondsLeft--
+        time.textContent = 'Time: ' +  secondsLeft;
+
+    }, 1000);
+}
+
+function btnClick() {
+    myTimer();
+    myObj();
+    myQuestion();
+  
+}
+
+var options = [
+  {
+    question: "What sound do cats make?",
+    answer: ["Moo", "Meow", "Woof", "Neigh"],
+  },
+
+  {
+    question: "Are dogs mamals?",
+    answer: ["yes", "no"],
+  },
+  {
+    question: "A group of birds are called...",
+    answer: ["A flock", "A pride", "A jet", "An aeroplane"],
+  },
+  {
+    question: "Which bird can fly backwards?",
+    answer: ["A penguin", "A hummingbird", "A crow", "A parrot"],
+  },
+];
+
+btn.addEventListener("click", btnClick);
